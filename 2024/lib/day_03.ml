fd4c56c5d7fa16026e049b0c731a43070a23f3cb
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-12-16 13:49:49 +1100

    Complete: 2024 Day 3 Part 2

--- 2024/lib/day_03.ml
+++ 2024/lib/day_03.ml
@@ -6,14 +6,29 @@ let mul_regex =
     ]
   |> compile
 
+let do_dont_regex =
+  let open Re in
+  alt
+    [
+      str "do()";
+      str "don't()";
+      seq
+        [
+          str "mul(";
+          repn digit 1 (Some 3);
+          str ",";
+          repn digit 1 (Some 3);
+          str ")";
+        ];
+    ]
+  |> compile
+
 let extract_ints_regex =
   let open Re in
   seq [ repn digit 1 (Some 3) ] |> compile
 
-let find_matches regex str = Re.all regex str
-
 let extract_ints match_string =
-  let matches = find_matches extract_ints_regex match_string in
+  let matches = Re.all extract_ints_regex match_string in
   let list =
     List.map
       (fun e -> int_of_string (List.nth (Array.to_list (Re.Group.all e)) 0))
@@ -21,10 +36,32 @@ let extract_ints match_string =
   in
   match list with a :: b :: _ -> (a, b) | _ -> (0, 0)
 
+let rec calc_part_2 matches running multiply =
+  match matches with
+  | a :: rest ->
+      check_match_state
+        (List.nth (Array.to_list (Re.Group.all a)) 0)
+        running multiply rest
+  | [] -> running
+
+and check_match_state reg_match running multiply rest =
+  match reg_match with
+  | "don't()" -> calc_part_2 rest running false
+  | "do()" -> calc_part_2 rest running true
+  | _ ->
+      if multiply = true then
+        calc_part_2 rest
+          (running
+          +
+          let a, b = extract_ints reg_match in
+          a * b)
+          multiply
+      else calc_part_2 rest running multiply
+
 let rec sum l = match l with [] -> 0 | h :: t -> h + sum t
 
 let part1 (input_text : string) : string =
-  let matches = find_matches mul_regex input_text in
+  let matches = Re.all mul_regex input_text in
   let res =
     List.map
       (fun e ->
@@ -34,4 +71,5 @@ let part1 (input_text : string) : string =
   in
   string_of_int (sum res)
 
-let part2 (input_text : string) : string = input_text
+let part2 (input_text : string) : string =
+  calc_part_2 (Re.all do_dont_regex input_text) 0 true |> string_of_int
